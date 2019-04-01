5dea1f22088edac4a73a3f54b717f124093c5a1a
Hannah <hstewart23@qub.ac.uk>
2019-04-01 03:06:08 +0100

    Content
    - caption content on index page carousel updated
    - things to do content updated
    -anchors added for things-to-do rather than separate pages
    - about content updated

--- buttonScript.js
+++
@@ -1,9 +0,0 @@
-
-
-function submitClick(form) {
-  alert("Submitted!");
-}
-
-function resetClick() {
-    return confirm("Are you sure you want to reset all text?");
-}
--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+
+function submitClick(form) {
+  alert("Submitted!");
 }
+
+function resetClick() {
+    return confirm("Are you sure you want to reset all text?");
+}
+
+function hide (elements) {
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'none';
+  }
+}
+
+// 'self' means 'this' from current scope
+var cn = function(self) {
+    // render class 'nav-..' and get last number
+    return (self).className.substr(4);
+  },
+  tcw = $('.tab-content-wrapper');
+
+$('.tab-nav-wrapper ul li').hover(function() {
+  tcw.children().hide(); // hide all 'tabs' under this scope
+  tcw.children('.tab' + cn(this) + '-c').show(); // show current 'tab' by a class name
+});
